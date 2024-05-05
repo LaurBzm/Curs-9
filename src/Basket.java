fdebf4d6729db4deb3bed360c162a286ca82d5c6
laurl <laurlumezeanu@gmail.com>
2024-05-05 17:07:45 +0300

    first commit

--- src/Basket.java
+++ src/Basket.java
@@ -1,27 +1,39 @@
 import java.util.ArrayList;
 
 public class Basket<Property> {
-    private String taste;
-    private boolean booked;
-    private String color;
-    private String size;
+
+    private boolean fruitBasket;
 
     public <Fruit> void main(String[] args) {
-        ArrayList<Basket<Property>> fruitBasket = new ArrayList<>();
-        String color = "Red";
-        String size = "Medium";
-        String taste = "Sweet";
-            this.color = color;
-            this.size = size;
-            this.taste = taste;
-            this.booked = false;
-            fruitBasket.add(this);
-        }
-            boolean truth;
+        ArrayList<String> fruitBasket = new ArrayList<>();
+        fruitBasket.add("Mar");
+        fruitBasket.add("Banana");
+        fruitBasket.add("Portocala");
+
+        this.fruitBasket = truth;
+        fruitBasket.add(this.toString());
+    }
+
+    boolean truth;
     ArrayList<Property> properties;
 
     public void Basket() {
         truth = false;
         properties = new ArrayList<Property>();
+        boolean b1 = false;
+        boolean b2 = true;
+
+        String s = new String(b1 + "" + b2);
+
+        s = s.replaceAll("false", "f");
+        s = s.replaceAll("true", "t");
+        class Returns {
+            public static void main(String args[]) {
+
+                String fruitBasket = new String();
+
+                System.out.println(fruitBasket.indexOf(0));
+            }
+        }
     }
 }
